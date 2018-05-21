8f2134f33c94631670b184b6e169bbf18f04259e
Jhey <macarubbo.david@gmail.com>
2018-05-21 12:28:16 +0800

    Added migration files to app

---
+++ database/migrations/2018_05_21_041911_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('company');
+            $table->integer('dosage');
+            $table->integer('generic');
+            $table->integer('pack_size');
+            $table->decimal('pack_price', 8, 2);
+            $table->decimal('unit_price', 8, 2);
+            $table->integer('status');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
